9b00dbdac1fa62ef7891a414da559d9aeed37f95
Katai_Phommachanh <kataix2@gmail.com>
2021-02-21 23:25:55 +0100

    implementing add invite friend function

--- app/src/main/java/com/example/travelbuddyv2/adapter/UserAdapter.java
+++ app/src/main/java/com/example/travelbuddyv2/adapter/UserAdapter.java
@@ -3,15 +3,27 @@ package com.example.travelbuddyv2.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.travelbuddyv2.R;
 import com.example.travelbuddyv2.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.io.FileReader;
 import java.util.List;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
@@ -33,13 +45,39 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull UserHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final UserHolder holder, int position) {
 
-        User user = users.get(position);
+        final User user = users.get(position);
 
         holder.tvFriendName.setText(user.getName());
         holder.tvFriendEmail.setText(user.getEmail());
         holder.img.setImageResource(holder.itemView.getResources().getIdentifier("@drawable/email_confirmmation",null,holder.itemView.getContext().getPackageName()));
+
+        holder.btnInviteFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 FirebaseDatabase.getInstance().getReference().child("Invitation_Request")
+                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                        .child(user.getUser_id())
+                        .child("request_type").setValue("sent")
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                FirebaseDatabase.getInstance().getReference().child("Invitation_Request")
+                                        .child(user.getUser_id())
+                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .child("request_type").setValue("received")
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(holder.itemView.getContext(),"Sent success",Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        });
+
+            }
+        });
 
     }
 
@@ -52,6 +90,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
 
         private final ImageView img;
         private final TextView tvFriendEmail,tvFriendName;
+        private final Button btnInviteFriend;
 
 
         public UserHolder(@NonNull View itemView) {
@@ -60,6 +99,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
            img = itemView.findViewById(R.id.friendProfile);
            tvFriendEmail = itemView.findViewById(R.id.friendEmail);
            tvFriendName = itemView.findViewById(R.id.friendName);
+           btnInviteFriend = itemView.findViewById(R.id.btnInviteFriend);
 
         }
     }
